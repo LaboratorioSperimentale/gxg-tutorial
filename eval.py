a7b1b895ffcf7febd876f6c50cc916d9a4043740
malvinanissim <malvina.nissim@gmail.com>
2018-09-24 11:26:41 +0200

    eval script

---
+++ eval.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+
+import argparse, sys
+from pandas_ml import ConfusionMatrix
+from sklearn.metrics import precision_recall_fscore_support as score
+
+
+def preproc(infile):
+    y = []
+    
+    reader = infile.read().splitlines() 
+    for row in reader:
+        label = row.split('\t')[2]
+        y.append(label)
+
+    return y
+
+
+def eval(y_test, y_predicted):    
+    
+    precision, recall, fscore, _ = score(y_test, y_predicted)
+    print'\n     {0}   {1}'.format("0","1")
+    print'P: {}'.format(precision)
+    print'R: {}'.format(recall)
+    print'F: {}'.format(fscore)
+
+    mprecision, mrecall, mfscore, _ = score(y_test, y_predicted, average='macro')
+    print'\n MACRO-AVG'
+    print'P: {}'.format(mprecision)
+    print'R: {}'.format(mrecall)
+    print'F: {}'.format(mfscore)
+    
+    wprecision, wrecall, wfscore, _ = score(y_test, y_predicted, average='weighted')
+    print'\n WEIGHTED-AVG'
+    print'P: {}'.format(wprecision)
+    print'R: {}'.format(wrecall)
+    print'F: {}'.format(wfscore)    
+    
+    print'\n CONFUSION MATRIX:'        
+    print ConfusionMatrix(y_test, y_predicted)
+    
+    
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('test', help='haspeede test set')
+    parser.add_argument('predicted', help='system output')
+    args = parser.parse_args() 
+    
+    with open(sys.argv[1], 'r') as tf:
+        y_test = preproc(tf)
+        
+    with open(sys.argv[2], 'r') as pf:
+        y_predicted = preproc(pf)
+        
+    eval(y_test, y_predicted)
+            
+      
+  
