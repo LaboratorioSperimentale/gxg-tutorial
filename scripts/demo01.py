657e57871ea1f8f02ef2bd21d040b92126aad3dc
LaboratorioSperimentale <lilec.lab@unibo.it>
2023-12-06 20:37:01 +0100

    lesson

---
+++ scripts/demo01.py
@@ -0,0 +1,29 @@
+# Python 101
+
+## There are tree fundamental types of entities: number, strings and booleans
+
+print(3+7)
+
+print("Hello world")
+
+print(True)
+
+
+## We can assign values to variables
+
+n = 13
+name = "Ludovica"
+is_italian = True
+
+## and perform operations on them
+
+print(n+3)
+print(name[0]+".")
+
+
+## Things can get more complicated
+
+if is_italian:
+    print("Ciao", name, ", a casa tutto bene?")
+else:
+    print("Hey there", name, ", where are you from?")
